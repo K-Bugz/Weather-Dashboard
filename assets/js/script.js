adb3f8bd7ffaa980dfeb74bc4c8a8e799ea5e9c3
kbugz <kbugusky@gmail.com>
2022-01-29 12:02:02 -0600

    script JS added

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Glovbal Variables 
+// Walter(TA) said to keep api key handy   fd491a535e375952c287ac394ad9eeea
+var apiKey = "fd491a535e375952c287ac394ad9eeea";
+var returnDataFromApi = JSON.stringify // JSON object
+var apiCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}";   // something like this. 
+var dates = [];  // a list that holds several objects
+// var
+// var
+
+// HTML variables 
+var inputBoxEl = fixLater;
+var searchInput = document.getElementID()
