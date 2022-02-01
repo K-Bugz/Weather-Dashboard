5f40cfdc881e50c1f8d55edf3bc7c8472c46c566
kbugz <kbugusky@gmail.com>
2022-02-01 15:51:53 -0600

    api semi working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,52 @@
-// Glovbal Variables 
-// Walter(TA) said to keep api key handy   fd491a535e375952c287ac394ad9eeea
-var apiKey = "fd491a535e375952c287ac394ad9eeea";
+// Global Variables 
+const apiKey = "fd491a535e375952c287ac394ad9eeea"; // const b/c it never changes (ask tutor why we need this)
 var returnDataFromApi = JSON.stringify // JSON object
-var apiCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}";   // something like this. 
-var dates = [];  // a list that holds several objects
-// var
-// var
+var locations = []; // blank list of locations
+// HTML element variables 
+var searchInputEl = document.getElementById("search-input"); // grabbing the element
+var searchFormEl = document.getElementById("search-form"); // get el
 
-// HTML variables 
-var inputBoxEl = fixLater;
-var searchInput = document.getElementID()
+// functions
+function getLocation(event) { // Get latitude and longitude
+    event.preventDefault();
+    let geoLoco = searchInputEl.value;
+    let url = conCatAPI(geoLoco);
+    fetch(url).then(response => { // response from API is a string. We use json to make it an object
+        // console.log(response); // called a buffer
+        return response.json(); // parse into JSON object
+    }).then(cities => {
+        // console.log(cities); // array of top 5 locations
+        if (cities.length == 1) {
+            locations.push(cities[0]);
+        }
+        else { // modal 
+
+        }
+        // TODOs
+        weatherAPI(locations[0].lat, locations[0].lon);
+        console.log(locations[0]);
+
+    })
+}
+// function to concatenate API for location
+function conCatAPI(q) { // this calls get location and q is the geoloco from the form.
+    var url = "http://api.openweathermap.org/geo/1.0/direct?";
+    var limit = 5;
+    return url + "q=" + q + "&limit=" + limit + "&appid=" + apiKey; // concatended string will return.
+}
+// A function to get API call for Weather data
+function weatherAPI(lat, lon) { // template literal!!!
+    url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data.current.temp);
+    })
+}
+// Event listerners
+searchFormEl.addEventListener("submit", getLocation); // only passing the function
+
+// querySelector("#search-input") this is anotherway to do this.
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
